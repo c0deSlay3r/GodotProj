f8008917036ccf842e9a03cb76fea82a64d0741c
cdsl3r <cdsl3r@noreply.com>
2024-03-11 15:15:49 +0500

    added player movement

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -2,6 +2,30 @@ extends Area2D
 
 @export var speed: int = 400
 var screen_size: Vector2
+var velocity = Vector2.ZERO
+
+
+func clamp_player(delta):
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size) 
+
+
+func check_input():
+	var l_velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		l_velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		l_velocity.x -= 1
+	if Input.is_action_pressed("move_back"):
+		l_velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		l_velocity.y -= 1
+
+	if l_velocity.length() > 0:
+		velocity = l_velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,4 +35,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	check_input()
+	clamp_player(delta)
+
