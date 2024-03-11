b8aed62c8852099950a4783c4359699967590283
cdsl3r <cdsl3r@noreply.com>
2024-03-11 14:52:47 +0500

    repository initialized and player scene added

---
+++ scenes/Player/Player.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@export var speed: int = 400
+var screen_size: Vector2
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
